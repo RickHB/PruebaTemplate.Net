b74e8c4348f258e3f128acb45f69de718ee543e7
RickHB <159599411+RickHB@users.noreply.github.com>
2024-05-19 22:17:50 -0600

    Delete OneDrive/Desktop/PruebaDaimler/DaimlerFrontend directory
    Error

--- OneDrive/Desktop/PruebaDaimler/DaimlerFrontend/src/Api/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    REACT_APP_LOCAL_HOST: 'https://localhost:7254',
-    REACT_APP_VEHICLES: 'VehicleSql+Oracle',
-    REACT_APP_VEHICLESORACLE: 'VehicleOracle',
-
-  };
-  
-  export default config;
--- OneDrive/Desktop/PruebaDaimler/DaimlerFrontend/src/components/ComponentsGrid.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ComponentsGrid() {
-  return (
-    <div>
-      <h1>COMPONENTS</h1>
-    </div>
-  )
-}
